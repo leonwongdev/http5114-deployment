e8db76c6ed7228ad2b186ffe1f8405b510e0549c
Lap Wang Wong <leonwonglww@gmail.com>
2023-10-22 16:16:13 -0400

    Changed heading

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-window.onload = () => {
-  const breakProdH2 = document.getElementById("break-prod");
-  setInterval(() => {
-    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    console.log(randomColor);
-    breakProdH2.style.backgroundColor = "#" + randomColor;
-  }, 50);
-};
+
